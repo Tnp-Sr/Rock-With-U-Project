20dc8ac78ae5afca16bbad5e7acd75bf3b6405fb
Poly29887 <tanapon1325@gmail.com>
2021-12-10 17:03:00 +0700

    แก้ url

--- project/homepage/urls.py
+++ project/homepage/urls.py
@@ -1,3 +1,28 @@
 from django.urls import path
+from . import views
 
-urlpatterns = [] 
+urlpatterns = [
+  path('',views.index),
+  path('index/',views.index),
+  path('menu_P1/',views.menu_P1),
+  path('menu_P2/',views.menu_P2),
+  path('menu_P3/',views.menu_P3),
+  path('menu_P4/',views.menu_P4),
+  path('menu_P5/',views.menu_P5),
+  path('menu_P6/',views.menu_P6),
+  path('menu_P7/',views.menu_P7),
+  path('menu_P8/',views.menu_P8),
+  path('menuSearch/',views.menuSearch),
+  path('registerForm/',views.registerForm),
+  path('addUser',views.addUser),
+  path('loginForm/',views.loginForm),
+  path('login',views.login),
+  path('logout/',views.logout),
+  path('orderHistory/',views.orderHistory),
+  path('orderQueue/',views.orderQueue),
+  path('queueDetail',views.queueDetail),
+  path('orderInfo/',views.orderInfo),
+  path('cartDetails/',views.cartDetails),
+  path('thanks/',views.thanks),
+  path('test/',views.loginForm)
+] 
